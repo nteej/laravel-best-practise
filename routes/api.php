8dfb1e954c1fecb486528f129c46de87c0e9716f
Tharangaj <nteeje@gmail.com>
2023-12-20 18:53:58 +0400

    Location request Post API route implement

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiteController;
+use App\Http\Controllers\TaskController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,3 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('commodity-prices', [SiteController::class,'getLatestPrices'])->name('api.commodity_prices');
 Route::get('best-price', [SiteController::class,'getLatestPrices'])->name('api.bestprice');
 Route::get('locations', [SiteController::class,'getLocations'])->name('api.locations');
+Route::post('location', [SiteController::class,'storeLocation'])->name('api.location');
+
+//Route::post('finish-task/{task-id}', TaskController::class,'finishTask')->name('api.task.finish');
+Route::resource('tasks', TaskController::class);
